4ca91467bc3bcd02ec3e23214ae77410c7d7a92e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-07 20:02:54 +0100

    Update 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- script that creates a trigger that decreases the quantity of an item
-CREATE TRIGGER order_item AFTER INSERT
-ON orders FOR EACH ROW
-UPDATE items
-SET quantity = quantity - NEW.numbers WHERE name = NEW.item_name;
+CREATE TRIGGER order_item 
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
