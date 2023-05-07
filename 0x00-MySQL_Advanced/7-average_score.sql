a9eca5d2fc6cb59df652374e89586be9a1e083b3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-07 21:45:17 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,13 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student
-DROP PROCEDURE IF EXISTS AddBonus;
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+    IN user_id INT)
 BEGIN
-    DECLARE a FLOAT;
-    SET a = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
-    UPDATE users SET average_score = a WHERE id = user_id;
+    DECLARE av FLOAT;
+    SET av = (SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id);
+    UPDATE users SET average_score = av WHERE id=user_id;
 END
 $$
 DELIMITER ;
