10227abfb6409c2bbaa67851a28131795065e9d3
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-07 21:19:46 +0100

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE AddBonus(
     IN project_name VARCHAR(255), 
     IN score FLOAT)
 BEGIN
-    DECLARE project_id INT;
+    DECLARE p_id INT;
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
-    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1); 
-    INSERT INTO Corrections (user_id, project_id, score) values (user_id, project_id, score);
+    SET pid = (SELECT id FROM projects WHERE name = project_name LIMIT 1); 
+    INSERT INTO Corrections (user_id, project_id, score) values (user_id, p_id, score);
 END
