adfe5bfb56cdf2ac099d632d771cadc1cea44784
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-07 21:15:21 +0100

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,15 @@
 -- script that creates a stored procedure AddBonus
-CREATE PROCEDURE AddBonus(user_id, project_name, score)
+DROP PROCEDURE IF EXISTS AddBonus;
+CREATE PROCEDURE AddBonus(
+    IN user_id INT, 
+    IN project_name VARCHAR(255), 
+    IN score FLOAT)
 BEGIN
-  DECLARE project_id INT;
-  IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-  THEN
-      INSERT INTO projects (name) values (project_name);
-  END IF;
-  SET project_id = (SELECT id FROM projects WHERE name = project_name); 
-  INSERT INTO Corrections (user_id, project_id, score) values (user_id, project_id, score);
+    DECLARE project_id INT;
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1); 
+    INSERT INTO Corrections (user_id, project_id, score) values (user_id, project_id, score);
 END
