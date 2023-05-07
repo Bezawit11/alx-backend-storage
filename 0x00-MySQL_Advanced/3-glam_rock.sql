8276e0436d4312335e4486785018889768da3a0d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-07 13:20:40 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
 -- script that lists all bands with Glam rock as their main style
 SELECT band_name AS band_name, split - formed AS lifespan FROM metal_bands
+WHERE style='Glam rock'
 GROUP BY band_name ORDER BY lifespan DESC;
