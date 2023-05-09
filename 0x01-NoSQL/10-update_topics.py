3d0971714b528d7381a4f5a7be15ec1f304cfed0
Bezawit11 <socbeza13@gmail.com>
2023-05-09 00:57:42 -0700

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""10-update_topics.py"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    m = []
+    cursor = mongo_collection.find({})
+    for d in cursor:
+        m.append(d)
+    for c in m:
+        if c.get('name') == name:
+            myquery = {c.get('name'): c.get('topics', "")}
+    newvalues = { "$set": {name: topics}}
+    mongo_collection.update_one(myquery, newvalues)
