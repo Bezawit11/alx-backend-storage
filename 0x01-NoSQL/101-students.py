6d44c5aec6fb0245a57fc8f9eab4bd1a40342d91
Bezawit11 <socbeza13@gmail.com>
2023-05-09 11:58:43 -0700

    task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""101-students.py"""
+
+
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    m = []
+    k = []
+    d = {}
+    s = 0
+    a = 0
+    for i in mongo_collection.find({}):
+        d['name'] = i.get('name')
+            for j in i.get('topics'):
+                s += j['score']
+                a += 1
+            avg = s / a
+            d['averagescore'] = avg
+            m.append(d)
+            d = {}
+    return m
