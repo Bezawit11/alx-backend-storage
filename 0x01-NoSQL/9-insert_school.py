b1abd581c28e9ea907cc2bda439f713f99143719
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-09 08:19:46 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
     for key, value in kwargs.items():
-        _id1 = mongo_collection.insert_one({key: value})
-    return _id1
+        r_id1 = mongo_collection.insert_one({key: value})
+    return r_id1.get('_id')
