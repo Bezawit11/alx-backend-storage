db33b3c2ce29c8ae9f9b515b65955a1a87f88f4e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 21:31:57 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,7 +11,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: str | bytes | int | float):
         a = str(uuid.uuid1())
         self._redis.set(a, data)
         return a
