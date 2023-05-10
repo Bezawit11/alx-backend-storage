2c8f04fad4bc019cba1a1f6a8ee2378109a060c9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 13:59:51 +0100

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,1 +1,12 @@
--- 
+-- computes and store the average weighted score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    IN user_id INT)
+BEGIN
+    DECLARE av FLOAT;
+    SET av = (SELECT AVG(score) FROM corrections WHERE corrections.user_id=user_id);
+    UPDATE users SET average_score = av WHERE id=user_id;
+END
+$$
+DELIMITER ;
