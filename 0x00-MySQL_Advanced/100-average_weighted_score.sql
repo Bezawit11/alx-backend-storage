e0ca43c49780fae026925bf18fab61b4126eebc9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 16:11:05 +0100

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -11,6 +11,7 @@ BEGIN
     ON p.project_id = c.user_id
     WHERE c.user_id = user_id);
     UPDATE users SET average_score = avg_score WHERE id = user_id;
+    SELECT * From users WHERE id = user_id;
 END
 $$
 DELIMITER ;
