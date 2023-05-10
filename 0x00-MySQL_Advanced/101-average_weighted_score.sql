e9921103da1736d5b5d0fe2a1f204d7a5df841ca
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 19:45:40 +0100

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -2,20 +2,21 @@
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
--- BEGIN
+BEGIN
     DECLARE RowCnt INT;
     DECLARE avg_score FLOAT;
     DECLARE b INT = 1;
     SET RowCnt = (SELECT COUNT(*) FROM corrections);
     WHILE b <= RowCnt
-BEGIN    
-    SET avg_score = (SELECT SUM(c.score * p.weight) / SUM(p.weight)
-    FROM corrections c
-    INNER JOIN projects p
-    ON p.id = c.project_id)
-    WHERE c.user_id = b;
-    UPDATE users SET average_score = avg_score WHERE id = b;
-    SET b += 1;
+    BEGIN    
+        SET avg_score = (SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections c
+        INNER JOIN projects p
+        ON p.id = c.project_id)
+        WHERE c.user_id = b;
+        UPDATE users SET average_score = avg_score WHERE id = b;
+        SET b += 1;
+    END
 END
 $$
 DELIMITER ;
