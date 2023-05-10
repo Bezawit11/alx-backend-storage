3adbd662b73c7fee451248fc40e8c29e404b432a
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 16:23:38 +0100

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -8,10 +8,9 @@ BEGIN
     SET avg_score = (SELECT SUM(c.score * p.weight) / SUM(p.weight)
     FROM corrections c
     INNER JOIN projects p
-    ON p.project_id = c.user_id
+    ON p.id = c.project_id
     WHERE c.user_id = user_id);
     UPDATE users SET average_score = avg_score WHERE id = user_id;
-    SELECT * From users WHERE id = user_id;
 END
 $$
 DELIMITER ;
