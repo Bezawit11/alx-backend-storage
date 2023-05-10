28429bb99a8c1e0990b0256ca3506f8d0076d714
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-10 13:36:23 +0100

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
 -- creates a view that lists all students that have a score under 80
 DROP VIEW IF EXISTS need_meeting;
-CREATE VIEW need_meeting AS SELECT * students WHERE
+CREATE VIEW need_meeting AS SELECT name students WHERE
 score < 80 AND 
 (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
