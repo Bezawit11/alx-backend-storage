c875af71b1f121a63a861c758c762eb11f33f517
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-14 15:38:16 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,16 +14,23 @@ r = redis.Redis()
 def counter(method: Callable) -> Callable:
     """count how many times methods of the Cache class are called"""
     @wraps(method)
-    def wrapper(url):
+    def wrapper(url):  # sourcery skip: use-named-expression
         """"increments value everytime the functin in called"""
-        k = "count:{}".format(url)
-        r.incr(k)
-        v = r.get(k)
-        if v:
-            return v.decode('utf-8')
-        a = method(url)
-        r.setex(k, 10, a)
-        return a
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+        #k = "count:{}".format(url)
+        #r.incr(k)
+        #v = r.get(k)
+        #if v:
+         #   return v.decode('utf-8')
+        #a = method(url)
+        #r.setex(k, 10, a)
+        #return a
     return wrapper
 
 @counter
