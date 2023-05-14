cf5e457db9f0f3e5b55993f9d419e511ffdf5ce9
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-14 15:40:57 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,25 +16,19 @@ def counter(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(url):  # sourcery skip: use-named-expression
         """"increments value everytime the functin in called"""
-        r.incr(f"count:{url}")
-        cached_html = r.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        r.setex(f"cached:{url}", 10, html)
-        return html
-        #k = "count:{}".format(url)
-        #r.incr(k)
-        #v = r.get(k)
-        #if v:
-         #   return v.decode('utf-8')
-        #a = method(url)
-        #r.setex(k, 10, a)
-        #return a
+        k = "count:{}".format(url)
+        v = r.get(k)
+        if v:
+            return v.decode('utf-8')
+        a = method(url)
+        r.setex(k, 10, a)
+        return a
     return wrapper
 
 @counter
 def get_page(url: str) -> str:
     """Implementing an expiring web cache and tracker""""
     x = requests.get(url)
+    k = "count:{}".format(url)
+    r.incr(k)
     return x.text
